1a37acdc289c187e28a804edabccbe950ec27394
Pedro Henrique Ruas Maschio <72313309+PedroHRMaschio@users.noreply.github.com>
2022-09-01 16:17:18 -0300

    chegando na metade da lógica

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,8 @@ import './Button.css';
 // eslint-disable-next-line import/no-anonymous-default-export
 export default props => 
     <button
-    onClick={e => props.click && props.click(e.label)}
+    onClick={() => props.click && props.click(props.label)}
+    // onKeyDownCapture={e => props.click && props.click(props.label)}
     className={`
         button
         ${props.operation ? 'operation' : ''}
